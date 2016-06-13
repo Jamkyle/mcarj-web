31b3b3a69628a2bb27429e52ce6ee533faa8a7b3
Jamkyle <raha.aro@gmail.com>
2016-06-13 15:20:07 +0200

    connect

--- src/lib/js/connect.js
+++ src/lib/js/connect.js
@@ -1,9 +1,12 @@
 import io from 'socket.io-client'
 
+let port = 4200
+let host = 'http://localhost'
+
 // export const socket = io.connect('http://192.168.1.21:4200');
 // export const socket = io.connect('http://192.168.0.14:4200');
 // export const socket = io.connect('http://192.168.0.22:4200');
-export const socket = io.connect('http://localhost:4200');
+export const socket = io.connect(host+':'+port);
 
 socket.on('connect', function (message) {
 
